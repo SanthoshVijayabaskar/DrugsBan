6f59d34e3a6c329005747362b895332b30ca5fde
Santhosh Vijay <santhosh.vijayabaskar@wolterskluwer.com>
2016-03-23 00:05:47 +0530

    Launch Screen Basic Layout

--- app/src/main/java/app/drugban/com/drugban/Home.java
+++ app/src/main/java/app/drugban/com/drugban/Home.java
@@ -2,12 +2,24 @@ package app.drugban.com.drugban;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
+    Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnSearch = (Button)findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Thanks for Trying! Let's release in next 4 weeks!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

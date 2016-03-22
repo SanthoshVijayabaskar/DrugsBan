71d1daadec8458ef32d347fc9f125b7bb602218f
Santhosh Vijay <santhosh.vijayabaskar@wolterskluwer.com>
2016-03-22 23:22:31 +0530

    Initial commit - Project Structure

---
+++ app/src/main/java/app/drugban/com/drugban/Home.java
@@ -0,0 +1,13 @@
+package app.drugban.com.drugban;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
